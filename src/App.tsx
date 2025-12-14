a450547116c8f057e97e9b648d87e7a93d3ff076
gitdevjin <dev.hjshin@gmail.com>
2025-12-14 17:31:40 -0500

    ngrok-test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { toast } from "sonner";
-import { Button } from "./components/ui/button";
 import Router from "./router";
 import ModalProvider from "./provider/modal-provider";
 
--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,2 +1,2 @@
 export const API_SERVER_URL =
-  "http://ec2-3-15-169-139.us-east-2.compute.amazonaws.com";
+  "https://palindromically-gorgeous-sacha.ngrok-free.dev";
