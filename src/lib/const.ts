9c9d8cbebe394d3f470baccdd5016362532a8186
gitdevjin <dev.hjshin@gmail.com>
2025-12-29 17:09:40 -0500

    serverurl modified for production

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,7 +1,7 @@
-// export const API_SERVER_URL =
-//   "https://palindromically-gorgeous-sacha.ngrok-free.dev";
+export const API_SERVER_URL =
+  "https://palindromically-gorgeous-sacha.ngrok-free.dev";
 
-export const API_SERVER_URL = "http://localhost:3000";
+// export const API_SERVER_URL = "http://localhost:3000";
 
 export const QUERY_KEYS = {
   user: {
